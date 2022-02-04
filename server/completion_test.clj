9d46fae24a9ec4bf5f31a551921831d8a4253b0c
Michiel Borkent <michielborkent@gmail.com>
2022-02-04 21:10:12 +0100

    completion

--- server/completion_test.clj
+++ server/completion_test.clj
@@ -19,14 +19,15 @@
     (let [_content-length (binding [*in* out]
                             (read-line))
           {:keys [id method] :as json} (json/parse-stream out true)]
-      (println :json json)
-      (binding [*out* in]
-        (let [request (json/generate-string
-                       {:jsonrpc "2.0"
-                        :method "textDocument/completion"
-                        :params {:textDocument {:uri "foobar"}
-                                 :position {:line 1 :character 2}}})
-              cl (content-length request)]
+      (println :response json)
+      (let [request (json/generate-string
+                     {:jsonrpc "2.0"
+                      :method "textDocument/completion"
+                      :params {:textDocument {:uri "foobar"}
+                               :position {:line 1 :character 2}}})
+            cl (content-length request)]
+        (println :request request)
+        (binding [*out* in]
           (println (str "Content-Length: " cl))
           (println "")
           (println request)))
